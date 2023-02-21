16b73b39aad9ae2fa19eb3ba1504e58ab6f764e9
hanseok0291 <hanseok0291@settlebank.co.kr>
2023-02-21 16:45:02 +0900

    기프티몰 메인팝업 추가

--- HotdealFE/components/coupon_pub/common/Modal/MainPopup.js
+++ HotdealFE/components/coupon_pub/common/Modal/MainPopup.js
@@ -8,12 +8,18 @@ import styleCommon from "../../../../styles/coupon_pub/Common.module.css";
 import styleModal from "../../../../styles/coupon_pub/Modal.module.css";
 import styleSlick from "../../../../styles/coupon_pub/Slick.module.css";
 
-const MainPopup = ({ title, cancle = "취소", confirm = "확인", content, onClose }) => {
-	const btnClose = () => {
-		onClose();
-	}
+const MainPopup = ({
+  title,
+  cancle = "취소",
+  confirm = "확인",
+  content,
+  onClose,
+}) => {
+  const btnClose = () => {
+    onClose();
+  };
 
-	const settings = {
+  const settings = {
     dots: true,
     infinite: true,
     speed: 500,
@@ -22,7 +28,7 @@ const MainPopup = ({ title, cancle = "취소", confirm = "확인", content, onCl
     initialSlide: 0,
     arrows: false,
     autoplay: false,
-    autoplaySpeed: 3000
+    autoplaySpeed: 3000,
   };
 
   return (
@@ -32,34 +38,80 @@ const MainPopup = ({ title, cancle = "취소", confirm = "확인", content, onCl
       <div className={styleModal.modalDialog}>
         <div className={styleModal.modalContent}>
           <div className={styleModal.modalHeader}>
-						<button type="button" onClick={btnClose} className={`${styleCommon.btnIcon} ${styleModal.btnClose} ${styleModal.modalClose}`}>닫기</button>
+            <button
+              type="button"
+              onClick={btnClose}
+              className={`${styleCommon.btnIcon} ${styleModal.btnClose} ${styleModal.modalClose}`}
+            >
+              닫기
+            </button>
           </div>
           <Slider {...settings}>
-						<div>
-							<div className={styleModal.modalClick}>
-								<div className={styleModal.modalBody}>
-									<h3 className={`${styleModal.popupTitle} ${styleModal.popupTitle2}`}>
-										<p className={styleModal.popupSubTitle}>두근두근 설레는 시작</p>
-										<strong className={styleModal.popupMainTitle}>신학기 <span className={styleCommon.highlight}>20% 할인</span></strong>
-									</h3>
-									<p className={styleModal.modalImgBox}>
-										<img
-											src="../images/coupon/popup/newSchool.png"
-											alt="신학기 20% 할인"
-										/>
-									</p>
-								</div>
-								<div className={styleModal.modalFooter}>
-									<button
-										type="button"
-										className={`${styleModal.btn} ${styleModal.btnFull}`}
-									>
-										최저가에 선물하기
-									</button>
-								</div>
-							</div>
-						</div>
-					</Slider>
+            <div>
+              <div className={styleModal.modalClick}>
+                <div className={styleModal.modalBody}>
+                  <h3
+                    className={`${styleModal.popupTitle} ${styleModal.popupTitle2}`}
+                  >
+                    <p className={styleModal.popupSubTitle}>
+                      달콤한 딸기와 함께
+                    </p>
+                    <strong className={styleModal.popupMainTitle}>
+                      화이트데이 <br />
+                      <span className={styleCommon.highlight}>
+                        20% EVENT<i className={`${styleCommon.italic}`}>!</i>
+                      </span>
+                    </strong>
+                  </h3>
+                  <p className={styleModal.modalImgBox}>
+                    <img
+                      src="../images/coupon/popup/whiteDay.png"
+                      alt="신학기 20% 할인"
+                    />
+                  </p>
+                </div>
+                <div className={styleModal.modalFooter}>
+                  <button
+                    type="button"
+                    className={`${styleModal.btn} ${styleModal.btnFull}`}
+                  >
+                    최저가에 선물하기
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className={styleModal.modalClick}>
+                <div className={styleModal.modalBody}>
+                  <h3
+                    className={`${styleModal.popupTitle} ${styleModal.popupTitle2}`}
+                  >
+                    <p className={styleModal.popupSubTitle}>
+                      두근두근 설레는 시작
+                    </p>
+                    <strong className={styleModal.popupMainTitle}>
+                      신학기{" "}
+                      <span className={styleCommon.highlight}>20% 할인</span>
+                    </strong>
+                  </h3>
+                  <p className={styleModal.modalImgBox}>
+                    <img
+                      src="../images/coupon/popup/newSchool.png"
+                      alt="신학기 20% 할인"
+                    />
+                  </p>
+                </div>
+                <div className={styleModal.modalFooter}>
+                  <button
+                    type="button"
+                    className={`${styleModal.btn} ${styleModal.btnFull}`}
+                  >
+                    최저가에 선물하기
+                  </button>
+                </div>
+              </div>
+            </div>
+          </Slider>
         </div>
       </div>
     </div>

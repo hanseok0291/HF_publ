ea559900795ef042606889074727b862bd7bb973
hanseok0291 <hanseok0291@settlebank.co.kr>
2023-02-27 08:38:40 +0900

    PG 휴대폰결제 무신사 커스텀

--- apps/pg/spay/resources/js/design_musinsa.js
+++ apps/pg/spay/resources/js/design_musinsa.js
@@ -5,58 +5,27 @@
 var mobileKeyWords = new Array('iPhone', 'iPod', 'BlackBerry', 'Android', 'Windows CE', 'Windows CE;', 'LG', 'MOT', 'SAMSUNG', 'SonyEricsson', 'Mobile', 'Symbian', 'Opera Mobi', 'Opera Mini', 'IEmobile');
   for (var word in mobileKeyWords) {
     if (navigator.userAgent.match(mobileKeyWords[word]) != null) {
-			console.log('m');
 			$("body").removeClass("pc");
    		break;
   } else {
-		console.log('pc');
 		$("body").addClass("pc");
 	}
 }
 
-// 스크롤 시 헤더 동작
-// var didScroll;
-// var lastScrollTop = 0;
-// var delta = 5;
-// var headerHeight = $("#header").outerHeight();
-
-// $(window).scroll(function(){
-// 	didScroll = true;
-// });
-// setInterval(function() {
-// 	if (didScroll) {
-// 		hasScrolled();
-// 		didScroll = false;
-// 	}
-// }, 250);
-// function hasScrolled() {
-// 	var st = $(this).scrollTop();
-// 	// Make sure they scroll more than delta
-// 	if(Math.abs(lastScrollTop - st) <= delta) {
-// 		return;
-// 	}
-// 	// If they scrolled down and are past the header, add class .header-up.
-// 	// This is necessary so you never see what is "behind" the header.
-// 	if (st > lastScrollTop && st > headerHeight){
-// 		// Scroll Down
-// 		$("#header").removeClass("header-down").addClass("header-up");
-// 	} else {
-// 		// Scroll Up
-// 		if(st + $(window).height() < $(document).height()) {
-// 			$("#header").removeClass("header-up").addClass("header-down");
-// 		}
-// 	}
-// 	lastScrollTop = st;
-
-// 	// 스크롤 시 헤더 하단 선 추가
-// 	if(lastScrollTop > 0){
-// 		$("#header").css("border-bottom","1px solid #ececec");
-// 	} else {
-// 		$("#header").css("border-bottom","0");
-// 		$("#header").removeClass("header-up").addClass("header-down");
-// 	}
-// }
-
+// 바디 스크롤 제거/해제
+var scrollHeight = 0;
+function scrollOff(){
+	scrollHeight = $(document).scrollTop();
+	$("body").addClass("modal-open");
+	$("#wrap").css("position", "fixed");
+	$("#wrap").css("top", - scrollHeight);
+}
+function scrollOn(){
+	$("body").removeClass("modal-open");
+	$("#wrap").css("top", 0);
+	$("#wrap").css("position", "relative");
+	$(document).scrollTop(scrollHeight);
+}
 
 // form
 $(function(){
@@ -80,58 +49,7 @@ $(function(){
 	// IE9 이하 jquery.placeholder.js 적용
 	$("input, textarea").placeholder();
 
-	// 약관 동의 - 전체 선택 체크박스 클릭
-	$(".agree-terms .agree-all [type=checkbox]").click(function(){
-		var child = $(this).parents(".agree-all").next(".terms-list").find("[type=checkbox]");
-		// 전체 선택 체크박스가 체크된 경우
-		if($(this).is(":checked")){
-			$(child).prop("checked", true);
-		// 전체 선택 체크박스가 해제된 경우
-		} else {
-			$(child).prop("checked", false);
-		}
-	});
-	// 약관 동의 - 하위 체크박스 클릭
-	$(".agree-terms .terms-list [type=checkbox]").click(function(){
-		var all = $(this).parents(".terms-list").prev(".agree-all").find("[type=checkbox]");
-		var childCount = $(this).parents(".terms-list").find("[type=checkbox]").length; // 하위 체크박스 수
-		var childCheckedCount = $(this).parents(".terms-list").find("[type=checkbox]:checked").length; // 체크된 하위 체크박스 수
-		// 하위 선택 체크박스가 모두 체크된 경우
-		if(childCheckedCount == childCount){
-			$(all).prop("checked", true);
-		// 하위 선택 체크박스가 모두 체크되지 않은 경우
-		} else {
-			$(all).prop("checked", false);
-		}
-	});
-
-	// card active
-	$(".card-check [type=checkbox]").click(function(){
-		var checkedInput = $(".card-check-input").is(":checked"); // prop('checked');
-		if(checkedInput == true) {
-			$(this).parents(".card-check").addClass("active");
-		} else {
-			$(this).parents(".card-check").removeClass("active");
-		}
-	});
 });
-
-
-// 바디 스크롤 제거/해제
-var scrollHeight = 0;
-function scrollOff(){
-	scrollHeight = $(document).scrollTop();
-	$("body").addClass("modal-open");
-	$("#wrap").css("position", "fixed");
-	$("#wrap").css("top", - scrollHeight);
-}
-function scrollOn(){
-	$("body").removeClass("modal-open");
-	$("#wrap").css("top", 0);
-	$("#wrap").css("position", "relative");
-	$(document).scrollTop(scrollHeight);
-}
-
 
 // 모달(레이어 팝업), 배너, 팝오버
 $(function(){
@@ -139,13 +57,6 @@ $(function(){
 	$("[data-toggle='modal']").click(function(){
 		var openBtn = $(this);
 		var target =  $(this).attr("data-target");				// 모달 ID
-//		var targetBtn = $(target).find("button:first-child");	// 모달 내 첫번째 버튼
-//		$(target).show()										// 모달 열기
-//		if($(target).is("#commonAlert") || $(target).is("#commonConfirm")){ // 모달 내 첫번재 버튼 포커스
-//			$(targetBtn).focus();
-//		} else {												// 모달 포커스
-//			$(target).focus();
-//		}
 		$(target).show().focus();								// 모달 열기, 포커스
 		scrollOff();											// 바디 스크롤 제거
 
@@ -156,9 +67,7 @@ $(function(){
 
 		// 모달 닫기
 		$("[data-dismiss='modal']").click(function(){
-			//$(target).hide();									// 모달 닫기
 			$(openBtn).focus();									// 열기 버튼 포커스
-			//scrollOn();										// 바디 스크롤 제거 해제
 		});
 	});
 
@@ -166,7 +75,6 @@ $(function(){
 	$("[data-dismiss='modal']").click(function(){
 		var target = $(this).parents(".modal");
 		$(target).hide();										// 모달 닫기
-		//$(openBtn).focus();									// 열기 버튼 포커스
 		scrollOn();												// 바디 스크롤 제거 해제
 	});
 
@@ -182,7 +90,6 @@ $(function(){
 		$(target).hide();
 	});
 });
-
 
 // 레이아웃, 토글, 슬라이드
 $(function(){
@@ -233,44 +140,7 @@ $(function(){
 		fixedFooter();
 	});
 
-	// 아코디언 메뉴 펼치기
-	// var acodian = {
-
-	// 	click: function(target) {
-	// 		var _self = this,
-	// 			$target = $(target);
-	// 		$target.on('click', function() {
-	// 			var $this = $(this);
-	// 			if ($this.next('.accordion-item').css('display') == 'none') {
-	// 				$('.accordion-item').slideUp();
-	// 				_self.onremove($target);
-	
-	// 				$this.addClass('on');
-	// 				$this.next().slideDown();
-	// 			} else {
-	// 				$('.accordion-item').slideUp();
-	// 				_self.onremove($target);
-	
-	// 			}
-	// 		});
-	// 	},
-	// 	onremove: function($target) {
-	// 		$target.removeClass('on');
-	// 	}
-	
-	// };
-	// acodian.click('.accordion-title');
-
-	// 아코디언 메뉴
-	// $(".accordion-list li").eq(0).addClass("on").find(".list-body").addClass("active"); // 첫번째 아이템 활성
-	// $(".accordion-list .list-header a").click(function(){
-	// 	// 영역 열기/닫기
-	// 	var thisItem = $(this).parents("li");
-	// 	var otherItem = $(thisItem).siblings("li");
-	// 	$(thisItem).toggleClass("on").find(".list-body").addClass("active");
-	// 	$(otherItem).removeClass("on").find(".list-body").removeClass("active");
-
-	$(".accordion-list li").removeClass("active").find(".list-body").hide(); // 첫번째 아이템 활성
+	$(".accordion-list li").removeClass("active").find(".list-body").hide(); // 아이템 감추기
 	$(".accordion-list li").eq(0).addClass("active").find(".list-body").slideDown(300); // 첫번째 아이템 활성
 	$(".accordion-list .list-header button").click(function(){
 		var thisItem = $(this).parents("li");
@@ -279,17 +149,4 @@ $(function(){
 		$(otherItem).removeClass("active").find(".list-body").slideUp(300);
 	});
 
-		// 목록 상단으로 스크롤
-		// setTimeout(function(){
-		// 	var thisOffset = $(thisItem).offset().top;
-		// 	var listOffset = $(".faq-list").offset().top + 10; // 상단 여백 10px
-		// 	$("html, body").animate({
-		// 		scrollTop: thisOffset - listOffset
-		// 	}, 200);
-		// }, 210);
 	});
-$('input').on('focus',function(){
-    var inputHeight=$(this).offset().top;
-    $("html, body").animate({ scrollTop: inputHeight}, 600);
-    return false;
- });

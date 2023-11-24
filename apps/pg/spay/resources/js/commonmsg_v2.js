0ab97cef7f6883ab3be64bc98df051240922d3ad
hanseok0291 <hanseok0291@gmail.com>
2023-11-24 11:07:21 +0900

    전결협 UI 추가

---
+++ apps/pg/spay/resources/js/commonmsg_v2.js
@@ -0,0 +1,21 @@
+/**
+ * Common Message Javascript
+ */
+
+var alertTitle = {
+				test1:	 				"test1",
+				test2:	 				"test2"
+};
+
+var alertMsg = {
+				test1:	 				"test1 test1",
+				test2:	 				"test2 test2"
+};
+
+var confirmTitle = {
+				closeWindow: 			"결제 종료"
+};
+
+var confirmMsg = {
+				closeWindow:			"결제를 취소하시겠습니까?"
+};

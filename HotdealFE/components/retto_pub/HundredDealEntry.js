cd584f5bb8190c0c93011679172eb672611932a5
swlee <swleebn@hecto.co.kr>
2024-03-28 10:41:10 +0900

    리또 내 100원딜 추가 응모권 모듈 수정

--- HotdealFE/components/retto_pub/HundredDealEntry.js
+++ HotdealFE/components/retto_pub/HundredDealEntry.js
@@ -8,9 +8,7 @@ const HundredDealEntry = () => {
     <div className={`${styleHundredDealEntry.borderBox}`}>
       <h2>매일 100원딜 응모해요</h2>
       <div className={styleHundredDealEntry.botContents}>
-        <p className={`${styleHundredDealEntry.leftContent} ${styleHundredDealEntry.ruby}`}>Lv. 1<em>루비</em></p>
-        {/* <p className={`${styleHundredDealEntry.leftContent} ${styleHundredDealEntry.emerald}`}>Lv. 2<em>에메랄드</em></p> */}
-        {/* <p className={`${styleHundredDealEntry.leftContent} ${styleHundredDealEntry.diamond}`}>MAX.<em>다이아</em></p> */}
+        <p className={`${styleHundredDealEntry.leftContent} ${styleHundredDealEntry.ruby}`}>머니 보관 성공</p>
         <p className={styleHundredDealEntry.rightContent}>리또 보상 응모권 <strong>0개</strong></p>
       </div>
     </div>

b7c72e0fe400fef315e42492c8c3c9ae2faf5e25
swlee <swleebn@hecto.co.kr>
2024-04-17 14:36:43 +0900

    파일 업로드

--- HotdealFE/pages/coupon_pub/mypage/wish.js
+++ HotdealFE/pages/coupon_pub/mypage/wish.js
@@ -11,6 +11,7 @@ import styleDefaultLayout from "../../../styles/coupon_pub/DefaultLayout.module.
 import Layout from "../../../components/common/Layout";
 import Header from "../../../components/coupon_pub/common/Header";
 import Footer from "../../../components/coupon_pub/common/Footer";
+import SwiperItem from "../../../components/coupon_pub/main/SwiperItem";
 
 import WishList from "../../../components/coupon_pub/list/WishList";
 
@@ -21,6 +22,51 @@ const Index = () => {
   const [isShowMypageBtn, setShowMypageBtn] = useState(false);
   const [isFixedTop, setFixedTop] = useState(false);
   const [isFixedBottom, setFixedBottom] = useState(true);
+
+  const listItem = [
+    {
+      infoImg: "https://tbezauth.settlebank.co.kr/theme/cm/product/BR00007_G00000117178.jpg",
+      infoBrand: "스타벅스",
+      infoProduct: "따뜻한 카페라떼 커플세트",
+      infoPrice: "10,000",
+      infoDcPercent: "25%",
+      infoDcPrice: "7,500",
+      infoBox1: false,
+      infoBox2: false,
+			addInfo1: true,
+			addInfo2: true
+    },
+    {
+      infoImg: "https://tbezauth.settlebank.co.kr/theme/cm/product/PB00145_20221006153424285.jpg",
+      infoBrand: "스타벅스",
+      infoProduct: "따뜻한 카페라떼 커플세트 따뜻한 카페라떼",
+      infoPrice: "10,000",
+      infoDcPercent: "25%",
+      infoDcPrice: "7,500",
+      infoBox1: false,
+      infoBox2: false,
+			addInfo3: true,
+			addInfo4: true,
+    },
+    {
+      infoImg: "https://tbezauth.settlebank.co.kr/theme/cm/product/PB00145_20221006133121119.jpg",
+      infoBrand: "스타벅스",
+      infoProduct: "시원하게 함께 세트",
+      infoDcPrice: "6,300",
+      infoBox1: false,
+      infoBox2: false,
+			addInfo5: true,
+			addInfo6: true,
+    },
+    {
+      infoImg: "https://tbezauth.settlebank.co.kr/theme/cm/product/PB00145_20221006134857069.png",
+      infoBrand: "스타벅스",
+      infoProduct: "시원하게 함께 세트 시원하 함께 세트",
+      infoDcPrice: "6,300",
+      infoBox1: false,
+      infoBox2: false,
+    },
+  ];
 
   return (
     <>
@@ -36,6 +82,16 @@ const Index = () => {
         >
           <WishList pageType="result" />
         </div>
+        <SwiperItem
+          title={["이런 상품은 어때요?"]}
+          moreBtnShow="false"
+          labelShow="true"
+          bgColor="#fbfbfb"
+          fontSize="17px"
+          paddingTop={34}
+          paddingBottom={50}
+          listItem={listItem}
+        />
         <Footer />
       </Layout>
     </>

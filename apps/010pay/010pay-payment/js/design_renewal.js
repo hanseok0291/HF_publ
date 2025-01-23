178cc5e8446f3c0fd26b9413b9b187c5bac8a9c3
swlee <swleebn@hecto.co.kr>
2025-01-23 15:06:36 +0900

    파일 업로드

--- apps/010pay/010pay-payment/js/design_renewal.js
+++ apps/010pay/010pay-payment/js/design_renewal.js
@@ -478,7 +478,9 @@ function modalOpenSlide(obj) {
     // 리스트에서 선택한 항목을 버튼에 반영
     $(".option-item button").on("click", function () {
         var selectedBank = $(this).text().trim(); // 선택한 이름 가져오기
-        selectBox.find(".label").text(selectedBank); // 버튼에 텍스트 업데이트
+        if(selectBox.find(".label").length > 0) {
+            selectBox.find(".label").text(selectedBank); // 버튼에 텍스트 업데이트
+        }
         selectBox.addClass("comp");
         $("#btnHidden").hide();
 

de0c041f2e54ca855a51e20d60f466dddf4a87a5
hanseok0291 <hanseok0291@hecto.co.kr>
2026-06-01 08:14:08 +0900

    퍼블 업로드

--- rround-fin-fe/src/pages/_document.js
+++ rround-fin-fe/src/pages/_document.js
@@ -4,7 +4,10 @@ export default function Document() {
   return (
     <Html lang="ko">
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
+        />
       </Head>
       <body>
         <Main />
